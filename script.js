3a12ec157e0d4e747d546033c851b069edd234f5
Nishantk2001 <nishant.k28101@gmail.com>
2024-02-20 15:59:16 +0530

    All Upadated

---
+++ script.js
@@ -0,0 +1,39 @@
+const searchbtn = document.getElementById("search-btn");
+const input = document.getElementById("city-input");
+const cityName = document.getElementById("cityName");
+const cityTime = document.getElementById("cityTime");
+const cityTemp = document.getElementById("cityTemp");
+const cityHumidity = document.getElementById("cityHumidity");
+const cityWind = document.getElementById("cityWind");
+const cityCloud = document.getElementById("cityCloud");
+const weatherIcon = document.getElementById("weatherIcon");
+
+async function getData(cityName) {
+  const weatherData = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=e5c5451a7b0945f2b4250238242002&q=${cityName}&aqi=yes`
+  );
+  return await weatherData.json();
+}
+
+searchbtn.addEventListener("click", async () => {
+  await fetchData();
+});
+
+input.addEventListener("keypress", async (e) => {
+  if (e.key === "Enter") {
+    await fetchData();
+  }
+});
+
+async function fetchData() {
+  const value = input.value;
+  const result = await getData(value);
+  // console.log(result);
+  cityName.innerText = `${result.location.name}, ${result.location.region}- ${result.location.country}`;
+  cityTime.innerText = `Local-Time: ${result.location.localtime} ⌛`;
+  cityTemp.innerText = `Temprature: ${result.current.temp_c} °C 🌡️`;
+  cityHumidity.innerText = `Humidity: ${result.current.humidity} % 🧑‍🤝‍🧑`;
+  cityWind.innerText = `Wind: ${result.current.wind_kph} kph 💨`;
+  cityCloud.innerText = `Cloud: ${result.current.cloud}% ⛅`;
+  weatherIcon.src = `${result.current.condition.icon}`;
+}
